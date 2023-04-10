d21d5bececb0a7368a50462cc571c4fbbb28eb02
YOUR NAMEMary <mz479@cornell.edu>
2023-04-10 15:40:35 -0400

    m1

--- db/init.sql
+++ db/init.sql
@@ -20,52 +20,52 @@ VALUES
 INSERT INTO
     albums(id, name, artist, year)
 VALUES
-    (1, 'Free time', 'Ruel', 2019);
+    (3, 'Free time', 'Ruel', 2019);
 
 INSERT INTO
     albums(id, name, artist, year)
 VALUES
-    (2, '25', 'Adele', 2015);
+    (4, '25', 'Adele', 2015);
 
 INSERT INTO
     albums(id, name, artist, year)
 VALUES
-    (1, '21', 'Adele', 2011);
+    (5, '21', 'Adele', 2011);
 
 INSERT INTO
     albums(id, name, artist, year)
 VALUES
-    (2, '30', 'Adele', 2021);
+    (6, '30', 'Adele', 2021);
 
 INSERT INTO
     albums(id, name, artist, year)
 VALUES
-    (1, '19', 'Adele', 2008);
+    (7, '19', 'Adele', 2008);
 
 INSERT INTO
     albums(id, name, artist, year)
 VALUES
-    (2, 'Narrated for You', 'Alec Benjamin', 2018);
+    (8, 'Narrated for You', 'Alec Benjamin', 2018);
 
 INSERT INTO
     albums(id, name, artist, year)
 VALUES
-    (2, 'Thank you Next', 'Ariana Grande', 2019);
+    (9, 'Thank you Next', 'Ariana Grande', 2019);
 
 INSERT INTO
     albums(id, name, artist, year)
 VALUES
-    (2, 'Lowlife Princess: Noir', 'BiBi', 2022);
+    (10, 'Lowlife Princess: Noir', 'BiBi', 2022);
 
 INSERT INTO
     albums(id, name, artist, year)
 VALUES
-    (2, 'When we all fall asleep, where do we go?', 'Billie Eilish', 2019);
+    (11, 'When we all fall asleep, where do we go?', 'Billie Eilish', 2019);
 
 INSERT INTO
     albums(id, name, artist, year)
 VALUES
-    (2, '24k', 'Bruno Mars', 2016);
+    (12, '24k', 'Bruno Mars', 2016);
 
 -- Album tags (connects types of genre with the album entry) --
 CREATE TABLE album_tags (
@@ -82,8 +82,40 @@ VALUES
     (1, 1, 1);
 INSERT INTO
     album_tags (id, album_id, tags_id)
-VALUES (2, 2, 2);
-
+VALUES
+    (2, 2, 2);
+INSERT INTO
+    album_tags (id, album_id, tags_id)
+VALUES
+    (3, 3, 3);
+INSERT INTO
+    album_tags (id, album_id, tags_id)
+VALUES
+    (4, 4, 4);
+INSERT INTO
+    album_tags (id, album_id, tags_id)
+VALUES
+    (5, 5, 5);
+INSERT INTO
+    album_tags (id, album_id, tags_id)
+VALUES
+    (6, 6, 6);
+INSERT INTO
+    album_tags (id, album_id, tags_id)
+VALUES
+    (7, 7, 7);
+INSERT INTO
+    album_tags (id, album_id, tags_id)
+VALUES
+    (8, 8, 8);
+INSERT INTO
+    album_tags (id, album_id, tags_id)
+VALUES
+    (9, 9, 9);
+INSERT INTO
+    album_tags (id, album_id, tags_id)
+VALUES
+    (10, 10, 10);
 
 -- Genre tags (all the different genres) --
 CREATE TABLE tags (
@@ -126,3 +158,18 @@ INSERT INTO
     tags (id, genre)
 VALUES
     (7, 'Alternative Pop');
+
+INSERT INTO
+    tags (id, genre)
+VALUES
+    (8, 'Alternative Pop');
+
+INSERT INTO
+    tags (id, genre)
+VALUES
+    (9, 'Alternative Pop');
+
+INSERT INTO
+    tags (id, genre)
+VALUES
+    (10, 'Alternative Pop');
